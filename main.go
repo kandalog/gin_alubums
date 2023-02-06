9a632ba077309280bdbd82d52243a91979872575
kandalog <asuasu.tee@gmail.com>
2023-02-06 19:40:31 +0900

    getとpostを追加

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// データの型を定義
+// JSONにparseされた時のkeyの名前も指定
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// サンプルデータ
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
 
 func main() {
-	fmt.Println("Hello Go")
+	router := gin.Default()
+
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+
+	// ":8080"
+	router.Run()
+}
+
+// 全ての情報を取得
+func getAlbums(c *gin.Context) {
+	// 構造体をJSONにシリアライズ
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// 情報を追加
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+	// albumsスライスに追加
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
